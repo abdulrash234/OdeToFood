87c4b23b31462bd4930d74e8c530cb2bbe0830d4
Abdulrashid Mohammed <abmoh@dfds.com>
2020-05-27 08:24:20 +0200

    Add project files.

---
+++ OdeToFood/Pages/Restaurants/Edit.cshtml.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using OdeToFood.Core;
+using OdeToFood.Data;
+
+namespace OdeToFood.Pages.Restaurants
+{
+    public class EditModel : PageModel
+    {
+        private readonly IRestaurantData _restauranData;
+        private readonly IHtmlHelper _htmlHelper;
+        
+        [BindProperty] 
+        public Restaurant Restaurant { get; set; }
+
+        public IEnumerable<SelectListItem> Cuisines { get; set; }
+
+        public EditModel(IRestaurantData restauranData, IHtmlHelper htmlHelper)
+        {
+            _restauranData = restauranData;
+            _htmlHelper = htmlHelper;
+        }
+        public IActionResult OnGet(int? restaurantId)
+        {
+            Cuisines = _htmlHelper.GetEnumSelectList<CuisineType>();
+            if (restaurantId.HasValue)
+            {
+                Restaurant = _restauranData.GetById(restaurantId.Value);
+            }
+            else
+            {
+                Restaurant = new Restaurant();
+            }
+
+
+            if (Restaurant == null)
+            {
+                return RedirectToPage("./NotFound");
+            }
+
+            return Page();
+        }
+
+        public IActionResult OnPost()
+        {
+            if (!ModelState.IsValid)
+            {
+                Cuisines = _htmlHelper.GetEnumSelectList<CuisineType>();
+                return Page();
+            }
+
+            if (Restaurant.Id > 0)
+            {
+                _restauranData.Update(Restaurant);
+            }
+
+            else
+            {
+                _restauranData.Add(Restaurant);
+            }
+            _restauranData.Commit();
+            TempData["Message"] = "Restaurant Saved";
+            return RedirectToPage("./Detail", new { restaurantId = Restaurant.Id });
+        }
+    }
+}
---
+++ OdeToFood/Pages/Restaurants/List.cshtml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Configuration;
+using OdeToFood.Core;
+using OdeToFood.Data;
+
+namespace OdeToFood.Pages.Restaurants
+{
+    public class ListModel : PageModel
+    {
+        private readonly IConfiguration _config;
+        private readonly IRestaurantData _restaurantData;
+
+        public string Message { get; set; }
+        public IEnumerable<Restaurant> Restaurants;
+        
+        [BindProperty]
+        public string SearchTerm { get; set; }
+        public ListModel(IConfiguration config, IRestaurantData restaurantData, IEnumerable<Restaurant> restaurants)
+        {
+            _config = config;
+           _restaurantData = restaurantData;
+        }
+
+        public void OnGet(string searchTerm)
+        {
+            Message = _config["Message"];
+            Restaurants = _restaurantData.GetRestaurantsByName(searchTerm);
+        }
+    }
+}
