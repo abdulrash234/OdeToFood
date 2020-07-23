e0f5f2fa8fa03f67d0b5c3d7143f9c2457286e22
Abdulrashid Mohammed <abmoh@dfds.com>
2020-07-23 12:26:52 +0200

    finalised odetofood

--- OdeToFood/Startup.cs
+++ OdeToFood/Startup.cs
@@ -25,7 +25,9 @@ namespace OdeToFood
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddMvc();
             services.AddRazorPages();
+            services.AddControllers();
             services.AddScoped<IRestaurantData, SqlRestaurantData>();
             services.AddDbContextPool<OdeToFoodDbContext>( options => options.UseSqlServer(Configuration.GetConnectionString("OdeToFoodDb")));
         }
